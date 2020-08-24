5bb9e75f9a511be2e695dcba5275614561f1c71b
sanaebadi <sanaebadi75@gmail.com>
2020-08-24 16:33:53 +0430

    added callback in ViewModel.kt

--- app/src/main/java/info/sanaebadi/dependecyinjection/ViewModel.kt
+++ app/src/main/java/info/sanaebadi/dependecyinjection/ViewModel.kt
@@ -1,14 +1,23 @@
 package info.sanaebadi.dependecyinjection
 
 class ViewModel {
+
+    var countUpdateCallback: ((Int) -> Unit)? = null
+        set(value) {
+            field = value
+            value?.invoke(counter.getCount())
+        }
+
     private val counter: Counter = SingleStepCounter()
 
     fun addClicked() {
         counter.increment()
+        countUpdateCallback?.invoke(counter.getCount())
     }
 
     fun subtractClicked() {
         counter.decrement()
+        countUpdateCallback?.invoke(counter.getCount())
     }
 
 }
