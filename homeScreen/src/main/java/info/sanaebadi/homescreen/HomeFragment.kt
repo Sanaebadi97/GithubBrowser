ecac2bb37d46f043d06831e1bc578249ef30d19b
Sana Ebadi <sanaebadi97@gmail.com>
2020-08-29 01:27:13 +0430

    added factory and viewModel in fragment

--- homeScreen/src/main/java/info/sanaebadi/homescreen/HomeFragment.kt
+++ homeScreen/src/main/java/info/sanaebadi/homescreen/HomeFragment.kt
@@ -1,13 +1,29 @@
 package info.sanaebadi.homescreen
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import info.sanaebadi.di.viewModel.AppViewModelFactory
 import info.sanaebadi.homescreen.databinding.ScreenHomeBinding
+import javax.inject.Inject
 
 class HomeFragment : Fragment() {
+
+    @Inject
+    lateinit var appViewModelFactory: AppViewModelFactory
+
+    private val homeViewModel: HomeViewModel by lazy {
+        ViewModelProvider(this, appViewModelFactory)[HomeViewModel::class.java]
+    }
+
+    override fun onAttach(context: Context) {
+        inject()
+        super.onAttach(context)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
