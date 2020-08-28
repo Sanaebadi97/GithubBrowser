3169027899e5be1e162370f043f67703b74358a6
Sana Ebadi <sanaebadi97@gmail.com>
2020-08-28 18:13:20 +0430

    added UserModel for user login

---
+++ githubapi/src/main/java/info/sanaebadi/githubapi/model/RepoApiModel.kt
@@ -0,0 +1,3 @@
+package info.sanaebadi.githubapi.model
+
+data class RepoApiModel(val id:Long )
---
+++ githubapi/src/main/java/info/sanaebadi/githubapi/model/UserApiModel.kt
@@ -0,0 +1,3 @@
+package info.sanaebadi.githubapi.model
+
+data class UserApiModel(val id:Long , val login:String)
