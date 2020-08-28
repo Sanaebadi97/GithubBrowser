a549fda838c42cec2fa40cf1d573075f7a08db74
Sana Ebadi <sanaebadi97@gmail.com>
2020-08-28 23:46:38 +0430

    added ViewModel

---
+++ homeScreen/src/main/java/info/sanaebadi/homescreen/HomeViewModel.kt
@@ -0,0 +1,30 @@
+package info.sanaebadi.homescreen
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import info.sanaebadi.homescreen.list.RepoItem
+import info.sanaebadi.repository.AppRepository
+import javax.inject.Inject
+
+class HomeViewModel @Inject constructor(
+    private val appRepository: AppRepository
+) : ViewModel() {
+
+    private val _viewState = MutableLiveData<HomeViewState>(HomeViewStateLoading)
+    val viewStateUpdates: LiveData<HomeViewState> = _viewState
+
+    init {
+        val topRepos = appRepository.getTopRepos()
+        _viewState.value = HomeViewStateLoaded(
+            repos = topRepos.map {
+                RepoItem(
+                    name = it.name,
+                    description = it.description,
+                    starsCount = it.stargazersCount,
+                    forkCount = it.forksCount
+                )
+            }
+        )
+    }
+}
