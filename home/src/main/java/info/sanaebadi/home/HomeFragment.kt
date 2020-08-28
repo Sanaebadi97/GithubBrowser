680e4117541ff44c0e2777fe6441e38715fedee9
Sana Ebadi <sanaebadi97@gmail.com>
2020-08-28 22:05:06 +0430

    added HomeFragment.kt

--- home/src/main/java/info/sanaebadi/home/HomeFragment.kt
+++ home/src/main/java/info/sanaebadi/home/HomeFragment.kt
@@ -1,2 +1,20 @@
 package info.sanaebadi.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import info.sanaebadi.home.databinding.ScreenHomeBinding
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = ScreenHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
