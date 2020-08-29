f7f6351eecab8a069639b4630e9cadef8add5520
Sana Ebadi <sanaebadi97@gmail.com>
2020-08-29 17:00:57 +0430

    added transaction  homeFragment in MainActivity.kt

--- app/src/main/java/info/sanaebadi/githubBrowser/MainActivity.kt
+++ app/src/main/java/info/sanaebadi/githubBrowser/MainActivity.kt
@@ -3,10 +3,17 @@ package info.sanaebadi.githubBrowser
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import info.sanaebadi.dependecyinjection.R
+import info.sanaebadi.homescreen.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState==null){
+            supportFragmentManager.beginTransaction()
+                .add(R.id.screen_container , HomeFragment())
+                .commit()
+        }
     }
 }
