f1627728231d2c2ad2c1721d3cfd4f09ae6c4f7b
Sana Ebadi <sanaebadi97@gmail.com>
2020-09-01 20:35:47 +0430

    added okHttp and Retrofit Provides

--- githubapi/src/main/java/info/sanaebadi/githubapi/GithubApiModule.kt
+++ githubapi/src/main/java/info/sanaebadi/githubapi/GithubApiModule.kt
@@ -1,10 +1,12 @@
 package info.sanaebadi.githubapi
 
-import dagger.Binds
+import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import okhttp3.Call
 import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -19,6 +21,29 @@ object GithubApiModule {
             .build()
     }
 
-    @Binds
-    fun bindGithubApi(mockGithubApi: MockGithubApi): GithubApi
+    @Provides
+    @JvmStatic
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .build()
+    }
+
+    @Provides
+    @JvmStatic
+    @Singleton
+    fun provideRetrofit(
+        moshi: Moshi,
+        callFactory: Call.Factory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .callFactory(callFactory)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+
+
+
 }
