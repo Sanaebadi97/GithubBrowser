133b432a154d91e05d52089db4a148bcff4fc109
Sana Ebadi <sanaebadi97@gmail.com>
2020-09-01 23:05:19 +0430

    added screenHomeFragment

---
+++ details/src/main/java/info/sanaebadi/details/RepoDetailsFragment.kt
@@ -0,0 +1,29 @@
+package info.sanaebadi.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import info.sanaebadi.details.databinding.ScreenDetailsBinding
+
+class RepoDetailsFragment : Fragment() {
+    companion object {
+        fun newInstance(repoOwner: String, repoName: String): RepoDetailsFragment {
+            val args = Bundle().apply {
+                putString("repo_name", repoName)
+                putString("repo_owner", repoOwner)
+            }
+            return RepoDetailsFragment().apply { arguments = args }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = ScreenDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
