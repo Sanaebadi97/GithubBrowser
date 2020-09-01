3367ecfcfbe3f76d198fc680a304c4eab0c0c8d9
Sana Ebadi <sanaebadi97@gmail.com>
2020-09-01 20:40:09 +0430

    deleted fake data in GithubApi.kt

--- githubapi/src/main/java/info/sanaebadi/githubapi/GithubApi.kt
+++ githubapi/src/main/java/info/sanaebadi/githubapi/GithubApi.kt
@@ -1,85 +1,8 @@
 package info.sanaebadi.githubapi
 
-import info.sanaebadi.githubapi.model.RepoApiModel
-import info.sanaebadi.githubapi.model.UserApiModel
-import javax.inject.Inject
-import javax.inject.Singleton
 
 interface GithubApi {
     suspend fun getTopRepositories(): TopReposSearchResult
 }
 
-@Singleton
-class MockGithubApi @Inject constructor() : GithubApi {
-    override suspend fun getTopRepositories(): TopReposSearchResult {
-        return TopReposSearchResult(
-            listOf(
-                RepoApiModel(
-                    id = 1L,
-                    name = "Mock Repo",
-                    description = "Mock Repo Description",
-                    owner = UserApiModel(id = 1L, login = "dagger"),
-                    stargazersCount = 1,
-                    forksCount = 4,
-                    contributorsUrl = "",
-                    createdDate = "1/1/2020",
-                    updatedDate = "2/2/2020"
 
-
-                ),
-                RepoApiModel(
-                    id = 1L,
-                    name = "Mock Repo",
-                    description = "Mock Repo Description",
-                    owner = UserApiModel(id = 1L, login = "dagger"),
-                    stargazersCount = 1,
-                    forksCount = 4,
-                    contributorsUrl = "",
-                    createdDate = "1/1/2020",
-                    updatedDate = "2/2/2020"
-
-
-                ),
-                RepoApiModel(
-                    id = 1L,
-                    name = "Mock Repo",
-                    description = "Mock Repo Description",
-                    owner = UserApiModel(id = 1L, login = "dagger"),
-                    stargazersCount = 1,
-                    forksCount = 4,
-                    contributorsUrl = "",
-                    createdDate = "1/1/2020",
-                    updatedDate = "2/2/2020"
-
-
-                ),
-                RepoApiModel(
-                    id = 1L,
-                    name = "Mock Repo",
-                    description = "Mock Repo Description",
-                    owner = UserApiModel(id = 1L, login = "dagger"),
-                    stargazersCount = 1,
-                    forksCount = 4,
-                    contributorsUrl = "",
-                    createdDate = "1/1/2020",
-                    updatedDate = "2/2/2020"
-
-
-                ),
-                RepoApiModel(
-                    id = 1L,
-                    name = "Mock Repo",
-                    description = "Mock Repo Description",
-                    owner = UserApiModel(id = 1L, login = "dagger"),
-                    stargazersCount = 1,
-                    forksCount = 4,
-                    contributorsUrl = "",
-                    createdDate = "1/1/2020",
-                    updatedDate = "2/2/2020"
-
-
-                )
-            )
-        )
-    }
-}
