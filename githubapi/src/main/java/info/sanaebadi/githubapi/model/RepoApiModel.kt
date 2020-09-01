b8d78ab9e4fb7f3ced4c285629d506e3386e9527
Sana Ebadi <sanaebadi97@gmail.com>
2020-09-01 20:26:00 +0430

    added Json correct name for model

--- githubapi/src/main/java/info/sanaebadi/githubapi/model/RepoApiModel.kt
+++ githubapi/src/main/java/info/sanaebadi/githubapi/model/RepoApiModel.kt
@@ -1,13 +1,15 @@
 package info.sanaebadi.githubapi.model
 
+import com.squareup.moshi.Json
+
 data class RepoApiModel(
     val id: Long,
     val name: String,
     val description: String,
     val owner: UserApiModel,
-    val stargazersCount: Int,
-    val forksCount: Int,
-    val contributorsUrl: String,
-    val createdDate: String,
-    val updatedDate: String
+    @Json(name = "stargazers_count") val stargazersCount: Int,
+    @Json(name = "forks_count") val forksCount: Int,
+    @Json(name = "contributors_url") val contributorsUrl: String,
+    @Json(name = "created_at") val createdDate: String,
+    @Json(name = "updated_at") val updatedDate: String
 )
