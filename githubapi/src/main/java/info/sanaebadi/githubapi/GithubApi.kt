8d1e8608f7ad451fdc2020dcd0976f0ee3ddcb55
Sana Ebadi <sanaebadi97@gmail.com>
2020-09-01 20:41:46 +0430

    added url in Get github api interface

--- githubapi/src/main/java/info/sanaebadi/githubapi/GithubApi.kt
+++ githubapi/src/main/java/info/sanaebadi/githubapi/GithubApi.kt
@@ -1,7 +1,10 @@
 package info.sanaebadi.githubapi
 
+import retrofit2.http.GET
+
 
 interface GithubApi {
+    @GET("search/repositories?q=language:kotlin&order=desc&sort=stars")
     suspend fun getTopRepositories(): TopReposSearchResult
 }
 
